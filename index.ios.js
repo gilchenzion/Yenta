1dd53cb71fb493e68b66b62a8aece674a3cfb630
gilchenzion <gchenzion@thrillist.com>
2015-05-02 15:48:07 -0400

    Boom facebook login

--- index.ios.js
+++ index.ios.js
@@ -5,49 +5,35 @@
 'use strict';
 
 var React = require('react-native');
+
+window.React = React;
+
 var {
   AppRegistry,
   StyleSheet,
-  Text,
-  View,
+  NavigatorIOS
 } = React;
+
+var Login = require('./Login');
 
 var Yenta = React.createClass({
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+Control+Z for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Login',
+          component: Login,
+        }}
+      />
     );
   }
 });
 
 var styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    flex: 1
+  }
 });
 
 AppRegistry.registerComponent('Yenta', () => Yenta);
